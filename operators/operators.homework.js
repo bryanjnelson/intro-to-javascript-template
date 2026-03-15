4adea0604213c6579bb31ce440985fe1c20a24c3
Bryan Nelson <1914742+bryanjnelson@users.noreply.github.com>
2026-03-15 20:19:31 +0000

    Creates folder structure and empty homework files - includes complete variables and data types homework - includes complete operators homework

---
+++ operators/operators.homework.js
@@ -0,0 +1,14 @@
+/*
+* Using a common theme (pick whatever you like) demonstrate two of the
+* following categories of operators:
+*
+* - Arithmetic Operators (+, -, *, /)
+* - Comparison Operators (===, !==, >, <, >=, <=)
+* - Logical Operators (&&, ||, !)
+* - String Operator (+)
+*
+* You will need to create variables to hold values and then log some
+* sort of output to the console that demonstrates the use of these operators.
+*/
+
+// Begin writing your code below
---
+++ variables-and-data-types/variables-and-data-types.homework.js
@@ -0,0 +1,13 @@
+/*
+* Using a common theme (pick whatever you like) create the following:
+* 
+* 1. A variable that holds a string
+* 2. A variable that holds a number
+* 3. A variable that holds a boolean
+* 4. A variable that holds undefined
+* 5. A variable that holds null
+* 
+* Then log each of these variables to the console showing the variable name and its value.
+*/
+
+// Begin writing your code below
